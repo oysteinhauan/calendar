2c01881dfa24b26398677f4821a98908de1117ed
Øystein Hauan <oysteibh@stud.ntnu.no>
2015-02-24 14:35:15 +0100

    lol

--- src/client/LoginTester.java
+++ src/client/LoginTester.java
@@ -8,8 +8,9 @@ public class LoginTester {
     public static void main(String[] args) {
         Login login = new Login("oysteibh");
         login.login("qwerty");
+        System.out.println(login.loggedin);
 
-        
+
         //login.login("qwerty");
 
     }
