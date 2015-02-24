b00a24de97897eda6cd02ec7f1639b9c6c35e5e8
Øystein Hauan <oysteibh@stud.ntnu.no>
2015-02-24 13:41:48 +0100

    La til flere klasser

--- src/client/Login.java
+++ src/client/Login.java
@@ -1,7 +1,43 @@
 package client;
 
+import database.Database;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 /**
  * Created by oysteinhauan on 24/02/15.
  */
 public class Login {
+
+    Database db;
+    String password;
+    boolean loggedin = false;
+
+    public Login(String username){
+        try {
+            db = new Database("all_s_gruppe40_calendar");
+            db.connectDb("all_s_gruppe40", "qwerty");
+            String sql = "SELECT password FROM users WHERE username = '" + username + "'";
+            ResultSet rs = db.readQuery(sql);
+            if (rs == null){
+                throw new IllegalArgumentException("Invalid username");
+            } else {
+                this.password = rs.getString("password");
+            }
+        } catch (SQLException e){
+        }
+
+    }
+
+    public void login(String password){
+        if (!password.equals(this.password)){
+            throw new IllegalArgumentException("Incorrect password");
+        }
+        else loggedin = true;
+    }
+
+
+
+
 }
---
+++ src/client/LoginTester.java
@@ -0,0 +1,13 @@
+package client;
+
+/**
+ * Created by oysteinhauan on 24/02/15.
+ */
+public class LoginTester {
+
+    public static void main(String[] args) {
+        Login login = new Login("oysteibh");
+        login.login("qwerty");
+        
+    }
+}
