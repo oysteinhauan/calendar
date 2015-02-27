5dc946af4f2fdd9ca1c5faa5344ca3633319cb64
henloef <henloef@hotmail.com>
2015-02-27 13:23:47 +0100

    change Person class to User

--- src/client/User.java
+++ src/client/User.java
@@ -1,21 +1,22 @@
 package client;
 
+
 import java.util.Date;
 
 /**
  * Created by oysteinhauan on 24/02/15.
  */
-public class Person {
+public class User {
 
     private String username, password, firstname, lastname, email, position;
-
-
-
-
+    database.Database db;
     private Date dateOfBirth;
 
+
+
+
     //lager tomt Personobjekt
-    public Person(String username){
+    public User(String username){
 
         this.username = username;
     }
@@ -30,38 +31,58 @@ public class Person {
     }
 
     public String getUsername() {
+
         return username;
     }
 
     public void setUsername(String username) {
+
         this.username = username;
     }
 
+//    public boolean isValidUsername(String username){
+//
+//
+//    }
+
     public String getFirstname() {
+
         return firstname;
     }
 
     public void setFirstname(String firstname) {
+
         this.firstname = firstname;
     }
 
     public String getLastname() {
+
         return lastname;
     }
 
     public void setLastname(String lastname) {
+
         this.lastname = lastname;
     }
 
     public String getFullName(){
+
         return "" + getFirstname() + " " + getLastname();
     }
 
     public Date getDateOfBirth() {
+
         return dateOfBirth;
     }
 
     public void setDateOfBirth(Date dateOfBirth) {
+
         this.dateOfBirth = dateOfBirth;
     }
+
+
+
+
+
+
 }
