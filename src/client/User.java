31ab800817a086129b79684a8bdd22b32ea0d34e
henloef <henloef@hotmail.com>
2015-03-02 12:43:48 +0100

    change Person class to User

--- src/client/User.java
+++ src/client/User.java
@@ -2,7 +2,10 @@ package client;
 
 import database.Database;
 
+import database.Database;
+
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Date;
 
 /**
@@ -10,86 +13,72 @@ import java.util.Date;
  */
 public class User {
 
-
-    private String username, password, firstname, lastname, email, position;
+    private String username;
+    private String password;
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String position;
     database.Database db;
     private Date dateOfBirth;
+    String sql;
 
 
+    public User(String username, String password, String firstname, String lastname, String email, String positoion, Date dateOfBirth) {
+        setUsername(username);
+        setPassword(password);
+        setFirstname(firstname);
+        setLastname(lastname);
+        setEmail(email);
+        setPosition(position);
 
-
-    //lager tomt Personobjekt
-    public User(String username){
-
-        this.username = username;
-        this.password = password;
-        this.firstname = firstname;
-        this.lastname = lastname;
-        this.email = email;
-        this.position = position;
-        this.dateOfBirth = dateOfBirth;
     }
 
+    
 
-
+    public String getPassword() {
+        return password;
+    }
 
     public void setPassword(String password) {
-
         this.password = password;
     }
 
     public String getUsername() {
-
         return username;
     }
 
     public void setUsername(String username) {
-
         this.username = username;
     }
 
-//    public boolean isValidUsername(String username){
-//
-//
-//    }
 
     public String getFirstname() {
-
         return firstname;
     }
 
     public void setFirstname(String firstname) {
-
         this.firstname = firstname;
     }
 
     public String getLastname() {
-
         return lastname;
     }
 
     public void setLastname(String lastname) {
-
         this.lastname = lastname;
     }
 
     public String getFullName(){
-
         return "" + getFirstname() + " " + getLastname();
     }
 
     public Date getDateOfBirth() {
-
         return dateOfBirth;
     }
 
     public void setDateOfBirth(Date dateOfBirth) {
-
         this.dateOfBirth = dateOfBirth;
-    }
-
-    public String getPassword() {
-        return password;
     }
 
     public String getEmail() {
@@ -104,11 +93,10 @@ public class User {
         return position;
     }
 
+
     public void setPosition(String position) {
         this.position = position;
     }
-
-
 
 
 
