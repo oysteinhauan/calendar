cdc0be72f6f675a4329ac5dcecf3a6ff956ee1e2
andrea rongved <andrealr@stud.ntnu.no>
2015-03-05 12:06:39 +0100

    Create createGroup-method. Test createGroupd-method.

--- src/client/Group.java
+++ src/client/Group.java
@@ -1,5 +1,7 @@
 package client;
 
+import database.Database;
+
 /**
  * Created by andrealouise on 24.02.15.
  */
@@ -8,5 +10,22 @@ public class Group implements AppointmentListener {
     @Override
     public void appointmentNotification(Appointment appointment) {
         //send notification til alle medlemmer
+    }
+
+    String groupname;
+    Database db;
+    String sql;
+
+    public Group(String groupname) {
+        this.groupname = groupname;
+    }
+
+
+    public void createGroup(Group group) {
+        db = new Database("all_s_gruppe40_calendar");
+        db.connectDb("all_s_gruppe40", "qwerty");
+        sql = ("INSERT INTO group_1 (name) values('" + (group.groupname) + "');");
+        db.updateQuery(sql);
+        db.closeConnection();
     }
 }
---
+++ src/client/GroupTest.java
@@ -0,0 +1,17 @@
+package client;
+
+/**
+ * Created by andrealouise on 05.03.15.
+ */
+public class GroupTest {
+
+    public static void main(String[] args) {
+        Group group = new Group("Power Puff");
+
+        group.createGroup(group);
+
+    }
+
+
+
+}
