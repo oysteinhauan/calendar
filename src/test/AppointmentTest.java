8f28a34b1da1047ebe703f994b0cc48434abfaa9
henloef <henloef@hotmail.com>
2015-03-06 10:22:12 +0100

    move tes classes to separate test package

---
+++ src/test/AppointmentTest.java
@@ -0,0 +1,47 @@
+package test;
+
+import client.Appointment;
+
+import java.sql.Timestamp;
+import java.util.Scanner;
+
+/**
+ * Created by oddma_000 on 27.02.2015.
+ */
+public class AppointmentTest {
+
+    public static void main(String[] args) {
+
+        //kjører et enkelt program  i consollen som lar deg legge til en ny appointment.
+
+        System.out.println("skriv inn ny appointment:");
+        Scanner sc = new Scanner(System.in);
+        String subject = "", description = "";
+        Timestamp start = null, end = null;
+
+        int duration = -1;
+        while (sc.hasNext()){
+
+            start = Timestamp.valueOf(sc.nextLine());
+            end = Timestamp.valueOf(sc.nextLine());
+
+            subject = sc.nextLine();
+            description = sc.nextLine();
+            break;
+
+
+
+        }
+
+
+
+        Appointment test = Appointment.createAppointment(start, end, subject, description, 20);
+        test.toString();
+
+
+
+
+
+    }
+
+}
---
+++ src/test/CalendarTest.java
@@ -0,0 +1,16 @@
+package test;
+
+import client.Calendar;
+
+/**
+ * Created by andrealouise on 03.03.15.
+ */
+public class CalendarTest {
+
+    public static void main(String[] args) {
+
+        Calendar cal = new Calendar("oysteibh");
+        cal.viewCalendar();
+
+    }
+}
