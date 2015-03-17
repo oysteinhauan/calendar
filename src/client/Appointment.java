143542a71a0fa57459b7bffb0ecd2c27971a20b7
Øystein Hauan <oysteibh@stud.ntnu.no>
2015-03-17 15:01:28 +0100

    fikset med grupper og litt sånt

--- src/client/Appointment.java
+++ src/client/Appointment.java
@@ -322,7 +322,7 @@ public class Appointment {
                 String sql2 = "DELETE from appointment where appointmentId = " + appointmentID + ";";
                 db.updateQuery(sql1);
                 db.updateQuery(sql2);
-                db.closeConnection();
+               // db.closeConnection();
             } else
                 throw new IllegalArgumentException("denne avtalen finnes ikke");
         }   catch (SQLException e){
---
+++ src/test/subGroupTest.java
@@ -0,0 +1,22 @@
+package test;
+
+import client.Group;
+import database.Database;
+
+/**
+ * Created by oysteinhauan on 17/03/15.
+ */
+public class subGroupTest {
+
+    /*public static void main(String[] args) {
+        Group group = new Group("Test");
+        Database db = new Database();
+        db.connectDb();
+
+        for (int i = 0; i < 30; i++) {
+            group.createSubGroup("Test" + i, db);
+            group = group.getSubGroups(group).get(0);
+
+        }
+    }*/
+}
